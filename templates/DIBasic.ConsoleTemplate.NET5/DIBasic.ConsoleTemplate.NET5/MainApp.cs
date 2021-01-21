0a06cabace32ec71c64c4939de91e59d80fce2d5
Henryk Wiśniewski <henryk.wisniewski@gmail.com>
2021-01-21 10:10:23 +0100

    Add project files.

---
+++ templates/DIBasic.ConsoleTemplate.NET5/DIBasic.ConsoleTemplate.NET5/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace DIBasic.ConsoleTemplate.NET5
+{
+    public class MainApp
+    {
+        private readonly ILogger<MainApp> _logger;
+
+        public MainApp(ILogger<MainApp> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Main application execution method.
+        /// </summary>
+        /// <param name="args">Arguments from command line parameters</param>
+        public async Task Run(string[] args)
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+}
---
+++ templates/DIBasic.ConsoleTemplate.NET5/DIBasic.ConsoleTemplate.NET5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using KeePass;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace DIBasic.ConsoleTemplate.NET5
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            // Default logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .CreateLogger();
+
+            // Dependency injection configuration
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(BuildConfig)
+                .ConfigureServices((context, services) =>
+                {
+                    services
+                        .AddTransient<MainApp>()
+                        .SetupKeePassServices(context.Configuration)
+                        .AddOptions();
+                })
+                .UseSerilog()
+                .Build();
+
+            // Actual main application start
+            try
+            {
+                var mainAppInstance = ActivatorUtilities.CreateInstance<MainApp>(host.Services);
+                await mainAppInstance.Run(args);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application crashed! Logging unhandled exceptions now.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        /// <summary>
+        /// All optional or required json files can be initialized here, so they will be accessible from <see cref="IConfiguration"/>
+        /// </summary>
+        /// <param name="builder"></param>
+        private static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables()
+                .AddUserSecrets(typeof(Program).Assembly, optional: true);
+        }
+    }
+}
